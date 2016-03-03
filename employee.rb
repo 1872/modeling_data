1f3a7c8002390bcf6f3ab794996f04964ae51d48
Peter Jang <peter.x.jang@gmail.com>
2016-03-02 19:26:18 -0600

    split classes and modules into separate files

--- employee.rb
+++ employee.rb
@@ -1,23 +1,6 @@
-employee1 = ["Majora", "Carter", 80000, true]
-employee2 = ["Danilo", "Campos", 70000, true]
-puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-puts ''
-
-employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
-employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
-puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
-puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
-puts ''
-
-employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
-employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
-puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-puts ''
-
 class Employee
   attr_reader :first_name, :last_name
+  attr_accessor :active
 
   def initialize(input_options)
     @first_name = input_options[:first_name]
@@ -34,13 +17,3 @@ class Employee
     @salary = 1.05 * @salary
   end
 end
-
-employee1 = Employee.new(last_name: "Carter", first_name: "Majora", salary: 80000, active: true)
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee3 = Employee.new(first_name: "Peter", last_name: "Jang", salary: 70000)
-employee1.print_info
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
-puts employee1.first_name.upcase
-
--- manager.rb
+++ manager.rb
@@ -1,43 +1,12 @@
-class Employee
-  attr_reader :first_name, :last_name
-  attr_accessor :active
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @salary = input_options[:salary]
-    @active = input_options[:active]
-  end
-
-  def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
-  end
-
-  def give_annual_raise
-    @salary = 1.05 * @salary
-  end
-
-  def first_name=(input_first_name)
-    @first_name = input_first_name
-  end
-end
+require './employee.rb'
+require './email_reporter.rb'
 
 class Manager < Employee
+  include EmailReporter
+
   def initialize(input_options)
     super(input_options)
     @employees = input_options[:employees]
-  end
-
-  def print_info
-    puts "Hello!"
-    super
-    puts "Goodbye!"
-  end
-
-  def send_report
-    puts "Sending email..."
-    # use email sending library
-    puts "Email sent!"
   end
 
   def give_all_raises
@@ -52,29 +21,3 @@ class Manager < Employee
     end
   end
 end
-
-employee1 = Employee.new(last_name: "Carter", first_name: "Majora", salary: 80000, active: true)
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-
-manager = Manager.new(
-  first_name: "Saron",
-  last_name: "Yitbarek",
-  salary: 100000,
-  active: true,
-  employees: [employee1, employee2]
-)
-
-employee1.print_info
-manager.give_all_raises
-employee1.print_info
-
-p employee1.active
-manager.fire_all_employees
-p employee1.active
-
-p manager.first_name
-manager.first_name = "SARON"
-p manager.first_name
-
-
-
